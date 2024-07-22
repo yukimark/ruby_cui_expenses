81c66e24b2e67dfdaaa46793d2c38229342b9066
yuki <mark2peace@gmail.com>
2024-07-23 07:46:03 +0900

    手動テスト用の.rbファイル更新

--- src/test.rb
+++ src/test.rb
@@ -1,35 +1,19 @@
 require "active_record"
-require 'pry-rails'
+require_relative 'module/db'
+require 'pry'
+require_relative 'models/spend'
 
-# データベースへの接続
-ActiveRecord::Base.establish_connection(
-  adapter:   'sqlite3',
-  database:  'test.sqlite3'
-)
+DB.connect
 
-# スキーマの設定
-# class InitialSchema < ActiveRecord::Migration[7.1]
-#   def self.up
-#     create_table :users do |t|
-#       t.string :name
-#     end
-#   end
-
-#   def self.down
-#     drop_table :users
-#   end
-# end
-
-# マイグレーション
-# InitialSchema.migrate(:up)
-
-class User < ActiveRecord::Base
-end
 
 # モデルを生成
-# User.create(name: 'hogehoge')
+Spend.create(category: '食費',
+             price: 123,
+             fixedcost: true,
+             deferredpay: true)
 
-binding.pry
 
 # 表示
-p User.find(1).name
+p Spend.last
+
+binding.pry
