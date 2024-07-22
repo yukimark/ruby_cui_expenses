0842073ec1c3f96c7a7a62811ce7a87356428ec3
yuki <mark2peace@gmail.com>
2024-07-22 22:21:11 +0900

    メニュー画面の試作実装

--- src/main.rb
+++ src/main.rb
@@ -1,8 +1,21 @@
 require_relative 'show_display_menu'
 
-main
-
 def main
-  show_display_menu
-  
+  loop do
+    menu_num = show_display_menu
+
+    case menu_num
+    when 1
+      p 'アプリを終了します'
+      break
+    when 2
+      p 'アプリを終了します'
+      break
+    when 3
+      p 'アプリを終了します'
+      break
+    end
+  end
 end
+
+main
