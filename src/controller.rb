071bdc697a1522a17eaa6f8b35827b79191d068d
yuki <mark2peace@gmail.com>
2024-07-23 21:41:06 +0900

    各項目出力時に横線を入れるようにした

--- src/controller.rb
+++ src/controller.rb
@@ -2,11 +2,15 @@ require_relative 'models/spend'
 require_relative 'module/view'
 
 def add_spend
+  View.line(2)
   spend = Spend.new()
   spend.category = View.spend_category
+  View.line(2)
   puts '金額を入力してください。'
   spend.price = gets.chomp.to_i
+  View.line(2)
   spend.fixedcost = View.boolean('固定費ですか?')
+  View.line(2)
   spend.deferredpay = View.boolean('カードなどの後払いですか?')
   spend.save
 end
