6880c02a61db5919735ce78675fa68abe0fb294a
yuki <mark2peace@gmail.com>
2024-07-25 05:36:20 +0900

    add:spend金額入力時のvalidate追加

--- src/models/spend.rb
+++ src/models/spend.rb
@@ -1,4 +1,9 @@
 require 'active_record'
 
 class Spend < ActiveRecord::Base
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 999999999 }
+
+  def input_price_validate
+    price.is_a?(Integer) && price >= 1 && price <= 999999999
+  end
 end
