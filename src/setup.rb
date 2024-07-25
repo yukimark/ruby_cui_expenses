1daa5d206f0a1f4528c50ca8a2642a665188f615
yuki <mark2peace@gmail.com>
2024-07-25 20:46:51 +0900

    add:curses初期設定追記

--- src/setup.rb
+++ src/setup.rb
@@ -1,10 +1,18 @@
 require_relative 'module/db'
 require 'i18n'
 require 'pry'
+require 'curses'
+
+CURSES_COLOR_RED = 1
+CURSES_COLOR_GREEN = 2
 
 def setup
   DB.connect
   i18n_setting
+  Curses.init_screen
+  Curses.start_color
+  Curses.init_pair(CURSES_COLOR_RED, Curses::COLOR_RED, Curses::COLOR_BLACK)
+  Curses.init_pair(CURSES_COLOR_GREEN, Curses::COLOR_GREEN, Curses::COLOR_BLACK)
 end
 
 private
