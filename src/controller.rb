c2a1715e5c9ff75f5cbb5323cf24af84f394c900
yuki <mark2peace@gmail.com>
2024-07-25 20:44:57 +0900

    rm:highlineで使ってた処理削除

--- src/controller.rb
+++ src/controller.rb
@@ -2,23 +2,17 @@ require_relative 'models/spend'
 require_relative 'module/view'
 
 def add_spend
-  View.line(2)
   spend = Spend.new
-  spend.category = View.spend_category
-  View.line(2)
+  spend.category = View::SpendView.spend_category
   loop do
     View.color_message('金額を入力してください。', :green)
     spend.price = gets.chomp.to_i
     break if spend.input_price_validate
 
-    View.line(2)
     View.color_message('金額は1から999,999,999までの数字を入力する必要があります。', :red)
   end
-  View.line(2)
   spend.fixedcost = View.boolean('固定費ですか?')
-  View.line(2)
   spend.deferredpay = View.boolean('カードなどの後払いですか?')
-  View.line(2)
   if View.confirm(content: spend, message: '上記の内容で登録しますか?') && spend.save
     View.color_message('保存しました。', :green)
   else
--- src/main.rb
+++ src/main.rb
@@ -5,29 +5,23 @@ require_relative 'setup'
 def main
   setup
   loop do
-    View.line(1)
     menu_str = View.top
 
     case menu_str
     when 'a'
       add_spend
-      View.line(2)
     when 's'
       spends = all_spend
       View.spend_index(spends)
-      View.line(2)
     when 'l'
       delete
     when ';'
-      View.line(1)
-      View.color_message('アプリを終了します。', :green)
       break
     end
   end
 end
 
 def delete
-  View.line(1)
   if Spend.count.zero?
     View.color_message('お小遣い帳のデータは0件です。', :red)
     return
@@ -37,10 +31,8 @@ def delete
   case menu_str
   when 'a'
     delete_spend_last
-    View.line(2)
   when 's'
     delete_spend_find_id
-    View.line(2)
   end
 end
 
