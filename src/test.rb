fd6dba32f1e7baf8a43954525cfca1a2fe23560c
yuki <mark2peace@gmail.com>
2024-07-25 20:46:29 +0900

    add:cursesテスト用のファイル作成

---
+++ src/test.rb
@@ -0,0 +1,24 @@
+require 'curses'
+require 'pry'
+
+Curses.init_screen
+begin
+  s = "Hello World!"
+  Curses.setpos(Curses.lines / 2, Curses.cols / 2 - (s.length / 2))
+  Curses.addstr(s)
+  Curses.refresh
+  Curses.getstr
+ensure
+  Curses.close_screen
+end
+
+Curses.init_screen
+begin
+  s = "うんこ!"
+  Curses.setpos(Curses.lines / 2, Curses.cols / 2 - (s.length / 2))
+  Curses.addstr(s)
+  Curses.refresh
+  Curses.getstr
+ensure
+  Curses.close_screen
+end
